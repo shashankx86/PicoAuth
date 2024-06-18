0ce65cb318fd0447be14657b5fa32e625dc232e9
shashankx86 <shashankspis20@gmail.com>
2024-06-18 22:17:06 +0000

    base: test work

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import data from './config/opt.json'; // Import JSON data
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+    
+    return () => clearInterval(timer);
+  }, []);
+
+  const fetchJson = () => {
+    fetch('./config/opt.json')
+    .then(response => {
+      return response.json();
+    }).then(data => {
+      setData(data);
+    }).catch((e) => {
+      console.log(e.message);
+    });
+  }
 
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <div id="local-time">
+        {time}, {data?.config}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
