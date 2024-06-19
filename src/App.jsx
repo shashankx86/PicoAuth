c3bd33c9fd10921720c83cd307260e6fbb09e65a
shashankx86 <shashankspis20@gmail.com>
2024-06-19 06:31:14 +0000

    base: fix json fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import data from './config/opt.json'; // Import JSON data
 import './App.css';
 
 function App() {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [config, setConfig] = useState(null);
 
   useEffect(() => {
     const timer = setInterval(() => {
       setTime(new Date().toLocaleTimeString());
     }, 1000);
-    
+
     return () => clearInterval(timer);
   }, []);
 
-  const fetchJson = () => {
-    fetch('./config/opt.json')
-    .then(response => {
-      return response.json();
-    }).then(data => {
-      setData(data);
-    }).catch((e) => {
-      console.log(e.message);
-    });
-  }
+  useEffect(() => {
+    fetch('../public/config.json')
+      .then(response => response.json())
+      .then(data => setConfig(data.config))
+      .catch(error => console.error('Error fetching config:', error));
+  }, []);
 
   return (
     <div>
       <div id="local-time">
-        {time}, {data?.config}
+        {time}
       </div>
+      {config && (
+        <div id="config">
+          <h2>Config Data:</h2>
+          <pre>{JSON.stringify(config.github.id, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
