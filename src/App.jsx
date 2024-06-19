cec71eba6496a6144bca626c52b6c019d31c99d4
shashankx86 <shashankspis20@gmail.com>
2024-06-19 17:51:09 +0000

    base: time show with AM/PM

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,12 @@ function App() {
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
+      setTime(new Date().toLocaleTimeString([],{
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+      }));
     }, 1000);
 
     return () => clearInterval(timer);
