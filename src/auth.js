9ab86f96769e8f760b3b0b75cac9eeb2cf50789c
shashankx86 <shashankspis20@gmail.com>
2024-06-20 15:37:31 +0000

    otplib: tested and worked

--- src/auth.js
+++ src/auth.js
@@ -1,1 +1,24 @@
-// wip
+const fs = require('fs');
+const path = require('path');
+const { totp } = require('./otplib');
+const { synchronisedTime } = require('./synchronisedTime');
+
+const codes = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
+let selectedIdx = 0;
+
+function main() {
+    setInterval(() => {
+        const code = codes[selectedIdx];
+        selectedIdx = (selectedIdx + 1) % codes.length;
+
+        const { password, expiry } = totp(synchronisedTime(), code.key, code.step, code.digits);
+
+        console.log(`\nCode Name: ${code.name}`);
+        console.log(`Password: ${password}`);
+        console.log(`${expiry} seconds remaining`);
+
+        setTimeout(() => {}, expiry * 1000);
+    }, 1000);
+}
+
+main();
---
+++ src/synchronisedTime.js
@@ -0,0 +1,5 @@
+function synchronisedTime() {
+    return Math.floor(Date.now() / 1000);
+}
+
+module.exports = { synchronisedTime };
