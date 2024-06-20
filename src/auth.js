4d77274c7890b1ce0fce9f91055fc294ef6ca572
shashankx86 <shashankspis20@gmail.com>
2024-06-20 08:51:49 +0000

    otplib: init sha1 for otp gen

--- src/auth.js
+++ src/auth.js
@@ -5,31 +5,27 @@ import { createDigest, createRandomBytes } from '@otplib/plugin-crypto';
 // Setup OTP instances
 const hotp = new HOTP({ createDigest });
 const totp = new TOTP({ createDigest });
-const authenticator = new Authenticator({
-  createDigest,
-  createRandomBytes,
-  keyDecoder,
-  keyEncoder
-});
+// const authenticator = new Authenticator({
+//   createDigest,
+//   createRandomBytes,
+//   keyDecoder,
+//   keyEncoder
+// });
 
 // Function to generate OTP based on service configuration
 function generateToken(serviceConfig) {
   const { id, type, key } = serviceConfig;
   let token;
-  let otpInstance;
 
   switch (type.toLowerCase()) {
     case 'hotp':
-      otpInstance = hotp;
       token = hotp.generate(key, 0);
       break;
     case 'totp':
-      otpInstance = totp;
       token = totp.generate(key);
       break;
     default:
-      otpInstance = authenticator;
-      token = authenticator.generate(key);
+      token = totp.generate(key);
   }
 
   return { id, token, type: type.toUpperCase() };
