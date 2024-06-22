bc051aeb0336d8ce01542aea08bd2ff08087a1f8
shashankx86 <shashankspis20@gmail.com>
2024-06-22 04:57:56 +0000

    base: added timer and copy button

--- src/components/OTPDisplay.vue
+++ src/components/OTPDisplay.vue
@@ -1,25 +1,46 @@
 <template>
-    <div class="otp-display">
-      <h1>{{ otp.password }}</h1>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      otp: {
-        type: Object,
-        required: true
-      }
+  <div class="otp-display">
+    <h1>{{ otp.password }}</h1>
+    <button @click="copyOTP" class="copy-button">Copy</button>
+    <p>{{ otp.expiry }} seconds remaining</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    otp: {
+      type: Object,
+      required: true
     }
-  };
-  </script>
-  
-  <style scoped>
-  .otp-display {
-    font-size: 3rem;
-    text-align: center;
-    margin-top: 20px;
+  },
+  methods: {
+    copyOTP() {
+      navigator.clipboard.writeText(this.otp.password)
+        .then(() => {
+          alert('OTP copied to clipboard!');
+        })
+        .catch(err => {
+          console.error('Failed to copy OTP:', err);
+          alert('Failed to copy OTP. Please try again.');
+        });
+    }
   }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.otp-display {
+  font-size: 3rem;
+  text-align: center;
+  /* padding-top: 20%; */
+  /* margin-top: 30px; */
+}
+
+.copy-button {
+  /* margin-top: 20px; */
+  padding: 8px 16px;
+  font-size: 1rem;
+  cursor: pointer;
+}
+</style>
