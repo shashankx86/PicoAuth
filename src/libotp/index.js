2630e92cc3f2f48da6c2792943421b32f864d9c8
shashankx86 <shashankspis20@gmail.com>
2024-06-22 04:48:36 +0000

    linotp: init hotp support

--- src/libotp/index.js
+++ src/libotp/index.js
@@ -1,22 +1,28 @@
 const { hmacSha1 } = require('./sha1');
 const { base32Decode } = require('./base32');
 
-function totp(time, key, stepSecs = 30, digits = 6) {
+function hotp(counter, key, digits = 6) {
     const decodedKey = base32Decode(key);
-    const timeBuffer = Buffer.alloc(8);
-    timeBuffer.writeUInt32BE(Math.floor(time / stepSecs), 4);
+    const counterBuffer = Buffer.alloc(8);
+    counterBuffer.writeUInt32BE(Math.floor(counter / Math.pow(2, 32)), 0);
+    counterBuffer.writeUInt32BE(counter % Math.pow(2, 32), 4);
 
-    const hmac = hmacSha1(decodedKey, timeBuffer);
+    const hmac = hmacSha1(decodedKey, counterBuffer);
     const offset = hmac[hmac.length - 1] & 0xF;
     const code = ((hmac[offset] & 0x7F) << 24 |
         (hmac[offset + 1] & 0xFF) << 16 |
         (hmac[offset + 2] & 0xFF) << 8 |
         (hmac[offset + 3] & 0xFF)) % (10 ** digits);
 
+    return code.toString().padStart(digits, '0');
+}
+
+function totp(time, key, stepSecs = 30, digits = 6) {
+    const counter = Math.floor(time / stepSecs);
     return {
-        password: code.toString().padStart(digits, '0'),
+        password: hotp(counter, key, digits),
         expiry: stepSecs - (time % stepSecs)
     };
 }
 
-module.exports = { totp };
+module.exports = { hotp, totp };
